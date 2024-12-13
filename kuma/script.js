9157dbb3c30b6eebcc957563b40cc5753d521c1d
dmilleza <darrenmilleza@gmail.com>
2024-12-12 16:32:48 -0800

    updated non wall3 stuff

--- kuma/script.js
+++
@@ -1,5 +0,0 @@
-const kuma = require('./data.json')
-
-const fast = kuma.frameData.dstance.sort((a, b) => a.startup - b.startup)
-
-console.log(fast)
--- yoshimitsu/script.js
+++
@@ -1,5 +0,0 @@
-const yoshi = require('./data.json')
-
-const fast = yoshi.frameData.sort((a, b) => a.startup - b.startup)
-
-console.log(fast)
